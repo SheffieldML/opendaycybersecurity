0df56fc3378c78d235c80eb4149d643d211f4648
Mike Smith <m.t.smith@sheffield.ac.uk>
2025-02-28 13:43:42 +0000

    Initial Idea

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'opendaycybersecurity',
+  packages = ['opendaycybersecurity'],
+  version = '0.1',
+  description = 'Tool for open day cyber security activity',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/lionfish0/opendaycybersecurity.git',
+  download_url = 'https://github.com/lionfish0/opendaycybersecurity.git',
+  keywords = ['openday','Sheffield','cybersecurity'],
+  classifiers = [],
+  install_requires=['numpy','flask','flask_cors'],
+  scripts=['bin/activity'],
+)
